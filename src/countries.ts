8ae0be2eadd24adf0d6007d207d4c51bd4e4989a
Yanina Ayala <yaninacayala@gmail.com>
2023-08-22 22:55:19 -0300

    Countries module fixed

--- src/countries.ts
+++ src/countries.ts
@@ -1,22 +1,32 @@
 const API_BASE_URL = `https://restcountries.com/v3.1/region/europe`;
+//const API_BASE_URL = new URL(`https://restcountries.com/v3.1/region/europe`);
 
 interface Country {
-    name: {
-        common: string;
-        official: string,
-        nativeName: Object
-    },
-    population: number;
+  name: {
+    common: string;
+    official: string;
+    nativeName: Object;
+  };
+  population: number;
 }
 
-function getAveragePopulation(): number {
-    // Completar esta función.
-
-    return 0;
+function getAveragePopulation(datos: any): number {
+  const populationArray = datos.map((country: any) => country.population);
+  const totalPopulation = populationArray.reduce(
+    (acum: number, valorAct: number) => (acum += valorAct)
+  );
+  const average = Math.round(totalPopulation / datos.length);
+  return average;
 }
 
 async function fetchData() {
-	
+  //fetch(API_BASE_URL).then((response) => response.json()).then((data) => getAveragePopulation(data));
+const response = await fetch(API_BASE_URL);
+const data = await response.json();
+const finalResult = getAveragePopulation(data);
+
+console.log(finalResult); 
+return finalResult
 }
 
 fetchData();
